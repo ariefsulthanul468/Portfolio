2e206171057c9689dc995a1df905a195418e1f2d
sulthanularief148 <ariefsulthanul468@gmail.com>
2023-05-02 07:25:02 +0530

    Animation added

--- js/scripts.js
+++ js/scripts.js
@@ -58,3 +58,20 @@ span.onclick = function() {
 box3.style.display = "none";
 $('body').css('overflow', 'visible');
 }
+
+// Scroll fade effect
+let elementsArray = document.querySelectorAll(".tile");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 100;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
+fadeIn();
