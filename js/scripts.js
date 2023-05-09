0da8bcb15514e3c92c9f1dba670a4379e1cd1a90
sulthanularief148 <ariefsulthanul468@gmail.com>
2023-05-09 17:51:27 +0530

    Pre final

--- js/scripts.js
+++ js/scripts.js
@@ -75,3 +75,9 @@ function fadeIn() {
     }
 }
 fadeIn();
+
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+// Set the current year in the footer
+document.getElementById('current-year').textContent = currentYear;
